2a3b53224640e735d618bea9df3eb50fd28e901f
fengyucai <i@caifengyu.net>
2016-01-27 02:30:49 +0800

    Matrix Transpose: triple representation

--- MatrixCompressedStorage/sparse_matrix_triple.cpp
+++ MatrixCompressedStorage/sparse_matrix_triple.cpp
@@ -2,22 +2,8 @@
 #include <cstdlib>
 #include <ctime>
 #define maxsize 32
-
-/*
-int A[10][10] = { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 2, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 3, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 4, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 5, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 6, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 7, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 8, 0, 0,
-                  8, 0, 0, 0, 0, 0, 0, 0, 9, 0,
-                  8, 0, 0, 0, 0, 0, 0, 0, 0, 10 };
-*/
 //定义int trimat[maxsize][3];
 //trimat[0][0]表示非零元素个数，trimat[0][1]表示矩阵行数，trimat[0][2]表示矩阵列数
-
 
 void create_trimat(int **A, int m, int n,  int B[][3]) {
     int k = 1;
@@ -33,6 +19,23 @@ void create_trimat(int **A, int m, int n,  int B[][3]) {
     B[0][0] = k-1;
     B[0][1] = m;
     B[0][2] = n;
+}
+
+void transpose(int A[][3], int At[][3]) {
+    At[0][0] = A[0][0];
+    At[0][1] = A[0][2];
+    At[0][2] = A[0][1];
+    if (A[0][0] > 0) {
+        int q = 1;
+        for (int col = 0; col < A[0][2]; col++) 
+          for (int p = 1; p <= A[0][0]; p++)
+            if (A[p][2] == col) {
+               At[q][0] = A[p][0];
+               At[q][1] = A[p][2];   // 转置矩阵的三元组按原矩阵的列优先方式存放
+               At[q][2] = A[p][1];
+               ++q;
+            }
+    }
 }
 
 void print(int B[][3]) {
@@ -56,6 +59,7 @@ void print(int B[][3]) {
         printf("%4d%4d%4d\n", B[k][1], B[k][2], B[k][0]);
 }
 
+
 int main() {
     int i, j, r;
     int **A = new int*[10];
@@ -68,11 +72,17 @@ int main() {
     for (i = 0; i < 10; i++)
       for (j = 0; j < 10; j++) {
           r = rand() % 10;
-          A[i][j] = (r == 9 ? (rand() % 10)+1 : 0);
+          A[i][j] = (r == 9 ? (rand() % 9)+1 : 0);
       }
     
     create_trimat(A, 10, 10, B);
     
     print(B);
+    //求转置矩阵B的三元组表示
+    int Bt[maxsize][3];
+    transpose(B, Bt);
+    printf("转置矩阵:\n");
+    print(Bt);
+    
     return 0;
 }
