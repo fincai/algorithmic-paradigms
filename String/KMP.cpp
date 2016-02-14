9aa2f58d407c1eea04dba2649db0116cf3267c0d
fengyucai <i@caifengyu.net>
2016-02-14 23:05:43 +0800

    KMP : build next[ ]

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -1,19 +1,36 @@
 #include <cstdio>
 #include <cstring>
 
-int * buildNext(char* P);
-
-int KMP_match(char* T, char* P) {
-    int *next = buildNext(P);
-    size_t n = strlen(T), i = 0;
+int* buildNext(char* P) {
     size_t m = strlen(P), j = 0;
+    int *next = new int[m];
+    int t = next[0] = -1;
+    while (j < m-1)
+      if (t < 0 || P[j] == P[t]) 
+        next[++j] = ++t;
+      else t = next[t];
+    return next;
+}
+
+int KMP_match(char* P, char* T) {
+    int *next = buildNext(P);
+    int n = (int)strlen(T), i = 0;
+    int m = (int)strlen(P), j = 0;
     while (j < m && i < n)
         if (j < 0 || T[i] == P[j])
         { i++; j++; }
         else j = next[j];
+    delete [ ] next;
     return i - j;
 }
 
 int main() {
-
+    char text[ ] = "acabaabaabcacaabc";
+    char pattern[ ] = "abaabcac";
+    size_t n = strlen(text), m = strlen(pattern);
+    int i = KMP_match(pattern, text);
+    if (i > (n-m)) 
+        printf("No match found.\n");
+    else
+        printf("Match found at index %d.\n", i);
 }
